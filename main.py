fab65dc7862d271ba58c87831a9fd1dd818c5376
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-09-28 10:45:54 +0300

    Human_check2

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ class Human:
         self.name = name
         self.surname = surname
         self.age = age
+    
+    def print(self):
+        return name 
+    
+    
 
 def Human_check():
     while True:
         try:
-            value = int(input())
+            value = int(input('Введите свой возраст(если Вам нет 14ти, вы не сможете пользоваться книгами: )'))
             if value > 13:
                 return value
             else:
-                print("Вам должно быть больше 14ти.")
-        
+                print("Вам должно быть больше 14-ти.")
         except Exception as e: 
             print(e)
+    return value
         
 
-
+name, surname = input('Напиши своё имя '), input('Напиши свою фамилию ') 
+age = Human_check()
+h1 = Human(name, surname, age)
+print(h1.print())
