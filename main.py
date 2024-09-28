0febd5a3ee044eb920e818bfb20cdb2b19cab6f9
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-09-28 10:17:17 +0300

    Human_check

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 class Human:
-    def __init__(self,name, surname, age):
+    def __init__(self, name, surname, age):
         self.name = name
         self.surname = surname
         self.age = age
+
+def Human_check():
+    while True:
+        try:
+            value = int(input())
+            if value > 13:
+                return value
+            else:
+                print("Вам должно быть больше 14ти.")
+        
+        except Exception as e: 
+            print(e)
+        
+
 
