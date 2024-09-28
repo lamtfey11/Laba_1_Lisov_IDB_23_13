2b44fc487e3d55281a8267ec9d649da298bb56a1
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-09-28 11:07:59 +0300

    Check_name_surname_age

--- main.py
+++ main.py
@@ -5,24 +5,50 @@ class Human:
         self.age = age
     
     def print(self):
-        return name 
+        print(name, surname, age) 
     
     
 
-def Human_check():
+def Human_check_name():
     while True:
         try:
-            value = int(input('Введите свой возраст(если Вам нет 14ти, вы не сможете пользоваться книгами: )'))
-            if value > 13:
-                return value
+            name = input('Введите своё имя: ')
+            if len(name) > 0:
+                print('Имя верное')
+                return name
+            else:
+                print("Имя должно иметь хотя бы одну букву.")
+        except Exception as n: 
+                print(n)
+
+def Human_check_surname():
+    while True:
+        try:
+            surname = input('Введите свою фамилию: ')
+            if len(surname) > 0:
+                print('Фамилия верная')
+                return surname
+            else:
+                print("Фамилия должна иметь хотя бы одну букву.")
+        except Exception as s: 
+                print(s)
+
+def Human_check_age():
+    while True:
+        try:
+            age = int(input('Введите свой возраст(если Вам нет 14ти, вы не сможете пользоваться книгами: '))
+            if age > 13:
+                print('Возраст подходит')
+                return age
             else:
                 print("Вам должно быть больше 14-ти.")
         except Exception as e: 
             print(e)
-    return value
+
+    h1 = Human(name, surname, age)
         
 
-name, surname = input('Напиши своё имя '), input('Напиши свою фамилию ') 
-age = Human_check()
+name = Human_check_name()
+surname = Human_check_surname()
+age = Human_check_age()
 h1 = Human(name, surname, age)
-print(h1.print())
