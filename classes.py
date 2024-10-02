4de0322bb24ca0a2385c7a4acdce510a13a72184
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-10-02 13:10:32 +0300

    School_Student_Club_classes

--- classes.py
+++ classes.py
@@ -9,3 +9,24 @@ class Reader(Human):
     def __init__(self, name, count, age):
         super().__init__(name, count)  
         self.age = age
+
+#ученик школы
+class School_Сhild(Human):
+    def __init__(self, name, count, age, school):
+        super().__init__(name, count)  
+        self.age = age
+        self.school = school
+
+#студент вуза
+class Student(Human):
+    def __init__(self, name, count, age, university):
+        super().__init__(name, count)  
+        self.age = age
+        self.university = university
+
+#участник читательского клуба
+class Club_Member(Human):
+    def __init__(self, name, count, age, club):
+        super().__init__(name, count)  
+        self.age = age
+        self.club = club
