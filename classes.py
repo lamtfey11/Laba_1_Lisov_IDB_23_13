b20e754d92edcfb44693364f053b0cb7315d0551
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-10-02 12:54:57 +0300

    Human_classes

--- classes.py
+++ classes.py
@@ -1,32 +1,8 @@
+#человек
 class Human:
-    def __init__(self, name, surname, age):
-        self.name = name
-        self.surname = surname
-        self.age = age
-    
-    def __str__(self):
-    #__str__ - специальный метод строкового представления объекта
-        return f'{self.name} {self.surname} {self.age}'
-    
-    def set_name(self, n):
-        self.name = n
+    def _init_(self, Name, Age, Count):
+        self.name = Name
+        self.age = Age 
+        self.count = Count
 
-    def set_surname(self, s):
-        self.surname = s
-    
-    def set_age(self, a):
-        self.age = a
 
-    def get_name(self):
-        print(self.name)
-
-    def get_surname(self):
-        print(self.surname)
-    
-    def get_age(self):
-        print(self.age)
-
-class Guest(Human):
-    def __init__(self, ID):
-        self.ID = ID        
-    
